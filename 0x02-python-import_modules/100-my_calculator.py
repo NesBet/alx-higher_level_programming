5047ddecb622465516e1bf02f6a1f99cb287c31a
kibet <nehmz254@gmail.com>
2023-08-11 16:21:52 +0300

    Fixing code

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
     nargs = len(sys.argv) - 1
     if nargs != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
     operation = sys.argv[2]
-    if operation != '+' and operation != '-' and operation != '*' and operation != '/':
+    if operation != '+' and operation != '-' 
+    and operation != '*' and operation != '/':
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
