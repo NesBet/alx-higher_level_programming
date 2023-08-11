9b08764122851cb90eded19029d05fd586c60e55
kibet <nehmz254@gmail.com>
2023-08-11 16:24:46 +0300

    Fixing code

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,21 +6,20 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    operation = sys.argv[2]
-    if operation != '+' and operation != '-' 
-    and operation != '*' and operation != '/':
-        print("Unknown operator. Available operators: +, -, * and /")
+    optn = sys.argv[2]
+    if optn != '+' and optn != '-'  and optn != '*' and optn != '/':
+        print("Invalid operator. Available operators: +, -, * and /")
         sys.exit(1)
 
     from calculator_1 import add, sub, mul, div
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
-    if operation == '+':
+    if optn == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
-    elif operation == '-':
+    elif optn == '-':
         print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif operation == '*':
+    elif optn == '*':
         print("{} * {} = {}".format(a, b, mul(a, b)))
     else:
         print("{} / {} = {}".format(a, b, div(a, b)))
