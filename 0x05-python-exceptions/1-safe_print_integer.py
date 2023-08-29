2fee37c6741061f5fc6486d18ed7a01e1821a449
kibet <nehmz254@gmail.com>
2023-08-29 21:32:36 +0300

    Fixing code

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
+    result = False
     try:
         print("{:d}".format(value))
+        result = True
     except:
-        return (False)
-    else:
-        return (True)
+        result = False
+    return (result)
