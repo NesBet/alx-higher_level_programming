21fb5d18f680da6d32e5b838c7f045a7bd0a816e
kibet <nehmz254@gmail.com>
2023-08-29 21:06:39 +0300

    Fixing code

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python3
-"""creates class Square with
-private instance attribute size and public instance method"""
+"""Example Google style docstrings."""
 
 
 class Square:
-    """defines class with instantiated and validated private instance attribute
-and public instance method."""
+    """The summary line for a class docstring should fit on one line."""
 
     def __init__(self, size=0):
-        self.__size = size
+        """The summary line for a class docstring should fit on one line."""
+        self.size = size
+
+    def area(self):
+        """The summary line for a class docstring should fit on one line."""
+        return (self._size * self._size)
 
     @property
     def size(self):
-        return(self.__size)
+        """The summary line for a class docstring should fit on one line."""
+        return self._size
 
     @size.setter
     def size(self, value):
-        if type(value) is not int:
+        """The summary line for a class docstring should fit on one line."""
+        if type(value) == int:
+            if value >= 0:
+                self._size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
             raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
-
-    def area(self):
-        """calculates and returns current square area"""
-        return(self.__size * self.__size)
