edfe9ef8cfab8581350fb5913fcf17f67c5e14af
kibet <nehmz254@gmail.com>
2023-08-29 14:32:28 +0300

    Fixing code

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""creates class square with private instance attribute size"""
+"""Creates class Square with private instance attribute size."""
+
 
 class Square:
-    """defines class as well as initializes pivate instance attribute size."""
+    """Defines class and instantiates private instance attribute size."""
+
     def __init__(self, size):
         self.__size = size
