3ae725b971f1f2c447e9159b772a1225ff50bc05
kibet <nehmz254@gmail.com>
2023-08-29 21:23:00 +0300

    Fixing codes

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,90 +1,77 @@
 #!/usr/bin/python3
-"""creates class Square with
-private instance attribute size and position and
-public instance methods to calculate area and print square"""
+
+
+"""The summary line for a class docstring should fit on one line.
+
+    If the class has public attributes, they may be documented here
+    in an ``Attributes`` section and follow the same formatting as a
+    function's ``Args`` section. Alternatively, attributes may be documented
+    inline with the attribute's declaration (see __init__ method below).
+
+    Properties created with the ``@property`` decorator should be documented
+    in the property's getter method.
+
+    Attributes:
+        attr1 (str): Description of `attr1`.
+
+    """
 
 
 class Square:
-    """defines class with private instance attributes size and position
-and public instance methods to calculate area and print square."""
+    """The summary line for a class docstring should fit on one line."""
 
     def __init__(self, size=0, position=(0, 0)):
-        """instantiates attribute size to 0 and position to (0, 0)"""
+        """The summary line for a class docstring should fit on one line."""
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        """get the private instance attribute size"""
-        return(self.__size)
-
-    @size.setter
-    def size(self, value):
-        """sets the private instance attribute size"""
-        if type(value) is not int:
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        """The summary line for a class docstring should fit on one line."""
+        return self.__size
 
     @property
     def position(self):
-        """gets the private instance attribute position"""
-        return(self.__position)
+        """The summary line for a class docstring should fit on one line."""
+        return self.__position
+
+    @size.setter
+    def size(self, value):
+        """The summary line for a class docstring should fit on one line."""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+                raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     @position.setter
     def position(self, value):
-        """sets the private instance attribute position"""
-        check = 0
-        while 1:
-            if type(value) is not tuple or len(value) is not 2:
-                check += 1
-                break
-            if type(value[0]) is not int or type(value[1]) is not int:
-                check += 1
-                break
-            if value[0] < 0 or value[1] < 0:
-                check += 1
-            break
-        if check is 0:
-            self.__position = value
+        """The summary line for a class docstring should fit on one line."""
+        str = "position must be a tuple of 2 positive integers"
+        if (type(value) is not tuple or len(value) != 2 or
+                type(value[0]) is not int or
+                type(value[1]) is not int or
+                value[0] < 0 or value[1] < 0):
+                raise TypeError(str)
         else:
-            raise TypeError("position must be a tuple of 2 positive integers")
+            self.__position = value
 
     def area(self):
-        """calculates and returns current square area"""
-        return(self.__size * self.__size)
+        """The summary line for a class docstring should fit on one line."""
+        return (self.__size ** 2)
 
     def my_print(self):
-        """prints square of size self.__size using #"""
-        if self.__size > 0:
-            for y in range(self.__position[1]):
-                print()
-            for column in range(self.__size):
-                for x in range(self.__position[0]):
-                    print(" ", end="")
-                for row in range(self.__size):
-                    print("#", end="")
-                print()
-        else:
+        if self.__size is 0:
             print()
+        else:
+            print('\n' * self.__position[1], end="")
+            for i in range(self.__size):
+                print(" " * self.__position[0], end="")
+                print("#" * self.__size, end="")
+                if i != self.__size - 1:
+                    print()
+        return ""
 
-    def my_print_string(self):
-        """formats returnable string like my_print"""
-        square_string = ""
-        if self.__size > 0:
-            for y in range(self.__position[1]):
-                square_string = square_string + "\n"
-            for row in range(self.__size):
-                for x in range(self.__position[0]):
-                    square_string = square_string + " "
-                for column in range(self.__size):
-                    square_string = square_string + "#"
-                if row is not (self.__size - 1):
-                    square_string = square_string + "\n"
-        return square_string
-
-    def __repr__(self):
-        """returns square representation as a string"""
-        return (self.my_print_string())
+    def __str__(self):
+        return str(self.my_print())
--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
+"""MagicClass Module"""
 import math
 
 
-class MagicClass:
-
-    """Class that stores the properties
-    of a circumference"""
+class MagicClass():
+    """ Magic class definitions """
     def __init__(self, radius=0):
-        self.__radius = 0
+        self._MagicClass__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
-        self.__radius = radius
+        self._MagicClass__radius = radius
 
-    """ Method that calculates the area of the circumference """
     def area(self):
-        return ((self.__radius ** 2) * math.pi)
+        return self._MagicClass__radius ** 2 * math.pi
 
-    """ Method that calculates the perimeter of a circumference """
     def circumference(self):
-        return (2 * math.pi * self.__radius)
+        return 2 * math.pi * self._MagicClass__radius
