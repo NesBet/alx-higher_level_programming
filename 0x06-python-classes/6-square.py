3e16dc42e7102b4db330f2f97c662407f3aea964
kibet <nehmz254@gmail.com>
2023-08-29 21:14:21 +0300

    Fixing code

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,70 +1,58 @@
 #!/usr/bin/python3
-"""creates class Square with
-private instance attribute size and position and
-public instance methods to calculate area and print square"""
+
+
+"""The summary line for a class docstring should fit on one line."""
 
 
 class Square:
-    """defines class with private instance attributes size and position
-and public instance methods to calculate area and print square."""
+    """The summary line for a class docstring should fit on one line."""
 
     def __init__(self, size=0, position=(0, 0)):
-        """instantiates attribute size to 0 and position to (0, 0)"""
+        """The summary line for a class docstring should fit on one line."""
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        """get the private instance attribute size"""
-        return(self.__size)
-
-    @size.setter
-    def size(self, value):
-        """sets the private instance attribute size"""
-        if type(value) is not int:
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
+        """The summary line for a class docstring should fit on one line."""
+        return self.__size
 
     @property
     def position(self):
-        """gets the private instance attribute position"""
-        return(self.__position)
+        """The summary line for a class docstring should fit on one line."""
+        return self.__position
+
+    @size.setter
+    def size(self, value):
+        """The summary line for a class docstring should fit on one line."""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+                raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     @position.setter
     def position(self, value):
-        """sets the private instance attribute position"""
-        check = 0
-        while 1:
-            if type(value) is not tuple or len(value) is not 2:
-                check += 1
-                break
-            if type(value[0]) is not int or type(value[1]) is not int:
-                check += 1
-                break
-            if value[0] < 0 or value[1] < 0:
-                check += 1
-            break
-        if check is 0:
-            self.__position = value
+        """The summary line for a class docstring should fit on one line."""
+        str = "position must be a tuple of 2 positive integers"
+        if (type(value) is not tuple or len(value) != 2 or
+                type(value[0]) is not int or
+                type(value[1]) is not int or
+                value[0] < 0 or value[1] < 0):
+                raise TypeError(str)
         else:
-            raise TypeError("position must be a tuple of 2 positive integers")
+            self.__position = value
 
     def area(self):
-        """calculates and returns current square area"""
-        return(self.__size * self.__size)
+        """The summary line for a class docstring should fit on one line."""
+        return (self.__size ** 2)
 
     def my_print(self):
-        """prints square of size self.__size using #"""
-        if self.__size > 0:
-            for y in range(self.__position[1]):
-                print()
-            for column in range(self.__size):
-                for x in range(self.__position[0]):
-                    print(" ", end="")
-                for row in range(self.__size):
-                    print("#", end="")
-                print()
-        else:
+        if self.__size is 0:
             print()
+        else:
+            print('\n' * self.__position[1], end="")
+            for i in range(self.__size):
+                print(" " * self.__position[0], end="")
+                print("#" * self.__size)
