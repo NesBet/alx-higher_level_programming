d6fbfb14a71fda4550069fd128ed931e57e2c80e
kibet <nehmz254@gmail.com>
2023-08-29 14:20:17 +0300

    Fixing code

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
+"""
+    Creates empty class square
+"""
+
 class Square:
     """
-    This class defines a square.
-
-    A square is a four-sided polyygon with all sides equal.
-
-    Attributes:
-        side_length (float): Length of sides.
-        
+        Defines an empty class.
     """
 
-    def __init__(self, side_length):
-        """
-        Initializes a square with given side length.
-
-        """
-        self.side_length = side_length
+    pass
